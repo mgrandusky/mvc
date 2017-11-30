4dba90517b0ae52435b509f39e7ca947cf35c9ff
Mason Grandusky <mgrandusky@gmail.com>
2017-11-30 15:07:50 -0500

    mvc framework

---
+++ mvc/application/models/UserModel.class.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class UserModel extends Model{
+
+
+    public function getUsers(){
+
+        $sql = "select * from $this->table";
+
+        $users = $this->db->getAll($sql);
+
+        return $users;
+
+    }
+
+}
